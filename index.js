02c14c70fd8de5628a1310105f7df8c745cdc171
Ben Schultz <ben@schultz.com>
2023-06-02 11:12:01 -0500

    Add an option to edit products.

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override')
 
 const Product = require('./models/product');
 
 // Parse extended urls and convert into a JavaScript object
 app.use(express.urlencoded({ extended: true }))
+
+// Override weird requests
+app.use(methodOverride('_method'))
 
 // Set view engine and path.
 app.set('views', path.join(__dirname, 'views'));
@@ -18,9 +22,38 @@ app.get('/', (req, res) => {
   res.send('TESTING');
 })
 
+app.get('/products/:id/edit', async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id)
+  res.render('products/edit', { product });
+})
+
+app.put('/products/:id', async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
+  console.log(req.body);
+  res.redirect(`/products/${product._id}`)
+
+})
+
 app.get('/products/new', (req, res) => {
   res.render('products/new');
 })
+
+app.post('/products', async (req, res) => {
+  const newProduct = new Product(req.body)
+  await newProduct.save();
+  res.redirect('/products');
+})
+
+
+// app.get('/products/new', (req, res) => {
+//   res.render('products/new', {  });
+// })
+
+
+
+
 
 app.get('/products', async (req, res) => {
   const products = await Product.find({})
